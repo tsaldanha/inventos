6e91411e80ff569b699fe69c0411f09b6667b46c
Tadeu Saldanha <tadeusaldanha@gmail.com>
2019-04-05 19:29:43 +0000

    Starting product model

---
+++ db/migrate/20190405181701_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :photo
+      t.float :price
+      t.integer :stock
+
+      t.timestamps
+    end
+  end
+end
