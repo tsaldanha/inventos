cbf16f5bd453f3975f8effd7d0750549af1229f8
Tadeu Saldanha <tadeusaldanha@gmail.com>
2019-04-08 02:37:44 +0000

    Layout and Seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  
+  resources :product
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'product#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product_list = [
+  [ "Minion 1", "Boneco dos Minions 1", "", 29.90, 10 ],
+  [ "Minion 2", "Boneco dos Minions 2", "", 39.90, 10 ],
+  [ "Minion 3", "Boneco dos Minions 3", "", 49.90, 10 ]
+]
+
+product_list.each do |name, description, photo, price, stock|
+  Product.create(name: name, description: description, photo: photo, price: price, stock: stock)
+end
+
