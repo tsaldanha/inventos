cd29a4e770331f8460ff715b29114475c5f8455b
Tadeu Saldanha <tadeusaldanha@gmail.com>
2019-04-15 01:44:17 +0000

    Adding Gmail port for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.smtp_settings = { port: 587 }
+
+
 end
