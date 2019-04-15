5e475651051bab0e145f7c7cb2ca407fa6b9ed5c
Tadeu Saldanha <tadeusaldanha@gmail.com>
2019-04-15 02:35:57 +0000

    Trying email without sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,15 +87,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'tadeusaldanha.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-
+  
 
 end
